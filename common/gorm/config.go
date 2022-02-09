3ac39195f267e831904e6a2c6114872d099e4e95
skywalkerwei <skywalkerwei@gmail.com>
2022-02-09 23:26:47 +0800

    fix

---
+++ common/gorm/config.go
@@ -0,0 +1,30 @@
+package gorm
+
+type Conf struct {
+	SlowThreshold int
+	IsOpenReadDb  bool
+	Write         struct {
+		Host               string
+		Port               int
+		User               string
+		Pass               string
+		Charset            string
+		DataBase           string
+		Prefix             string
+		SetMaxIdleConns    int
+		SetMaxOpenConns    int
+		SetConnMaxLifetime int
+	}
+	Read struct {
+		Host               string
+		Port               int
+		User               string
+		Pass               string
+		Charset            string
+		DataBase           string
+		Prefix             string
+		SetMaxIdleConns    int
+		SetMaxOpenConns    int
+		SetConnMaxLifetime int
+	}
+}
--- common/gorm/gorm.go
+++ common/gorm/gorm.go
@@ -1,49 +1,41 @@
 package gorm
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/plugin/dbresolver"
 	"time"
 )
 
-//Dbc ，
-func dbConn(User, Password, Host, Db string, Port int) *gorm.DB {
-	gormDb, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "User:Password@tcp(Host:Port)/Db?charset=utf8&parseTime=True&loc=Local", // DSN data source name
-		DefaultStringSize:         256,                                                                     // string 类型字段的默认长度
-		DisableDatetimePrecision:  true,                                                                    // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-		DontSupportRenameIndex:    true,                                                                    // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-		DontSupportRenameColumn:   true,                                                                    // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-		SkipInitializeWithVersion: false,
-	}), &gorm.Config{
+func GetSqlDriver(conf Conf) *gorm.DB {
+	var dbDialector gorm.Dialector
+	if val, err := getDbDialector("w", conf); err == nil {
+		dbDialector = val
+	}
+	gormDb, err := gorm.Open(dbDialector, &gorm.Config{
 		SkipDefaultTransaction: true,
 		PrepareStmt:            true,
 	})
 	if err != nil {
 		return nil
 	}
-	//sqlDB, _ := db.DB()
-	//// SetMaxIdleConns 用于设置连接池中空闲连接的最大数量。
-	//sqlDB.SetMaxIdleConns(10)
-	////// SetMaxOpenConns 设置打开数据库连接的最大数量。
-	//sqlDB.SetMaxOpenConns(100)
-	////// SetConnMaxLifetime 设置了连接可复用的最大时间。
-	//sqlDB.SetConnMaxLifetime(time.Hour)
-	readDbIsOpen := 1
 
-	if readDbIsOpen == 1 {
-		if val, err := getDbDialector(sqlType, "Read", dbConf...); err == nil {
+	if conf.IsOpenReadDb {
+
+		if val, err := getDbDialector("r", conf); err == nil {
 			dbDialector = val
 		}
 		resolverConf := dbresolver.Config{
 			Replicas: []gorm.Dialector{dbDialector}, //  读 操作库，查询类
 			Policy:   dbresolver.RandomPolicy{},     // sources/replicas 负载均衡策略适用于
 		}
-		err = gormDb.Use(dbresolver.Register(resolverConf).SetConnMaxIdleTime(time.Second * 30).
-			SetConnMaxLifetime(variable.ConfigGormv2Yml.GetDuration("Gormv2."+sqlType+".Read.SetConnMaxLifetime") * time.Second).
-			SetMaxIdleConns(variable.ConfigGormv2Yml.GetInt("Gormv2." + sqlType + ".Read.SetMaxIdleConns")).
-			SetMaxOpenConns(variable.ConfigGormv2Yml.GetInt("Gormv2." + sqlType + ".Read.SetMaxOpenConns")))
+		err = gormDb.Use(
+			dbresolver.Register(resolverConf).
+				SetConnMaxLifetime(60 * time.Second).
+				SetMaxIdleConns(conf.Read.SetMaxIdleConns).
+				SetMaxOpenConns(conf.Read.SetMaxOpenConns),
+		)
 		if err != nil {
 			return nil
 		}
@@ -59,15 +51,41 @@ func dbConn(User, Password, Host, Db string, Port int) *gorm.DB {
 	_ = gormDb.Callback().Update().Before("gorm:before_update").Register("UpdateBeforeHook", UpdateBeforeHook)
 
 	// 为主连接设置连接池(43行返回的数据库驱动指针)
-	if rawDb, err := gormDb.DB(); err != nil {
-		return nil, err
-	} else {
+	if rawDb, err := gormDb.DB(); err == nil {
 		rawDb.SetConnMaxIdleTime(time.Second * 30)
-		rawDb.SetConnMaxLifetime(variable.ConfigGormv2Yml.GetDuration("Gormv2."+sqlType+".Write.SetConnMaxLifetime") * time.Second)
-		rawDb.SetMaxIdleConns(variable.ConfigGormv2Yml.GetInt("Gormv2." + sqlType + ".Write.SetMaxIdleConns"))
-		rawDb.SetMaxOpenConns(variable.ConfigGormv2Yml.GetInt("Gormv2." + sqlType + ".Write.SetMaxOpenConns"))
-		return gormDb, nil
+		rawDb.SetConnMaxLifetime(60 * time.Second)
+		rawDb.SetMaxIdleConns(conf.Write.SetMaxIdleConns)
+		rawDb.SetMaxOpenConns(conf.Write.SetMaxOpenConns)
+		return gormDb
 	}
 
-	return db
+	return gormDb
+}
+
+// 获取一个数据库方言(Dialector),通俗的说就是根据不同的连接参数，获取具体的一类数据库的连接指针
+func getDbDialector(rw string, conf Conf) (gorm.Dialector, error) {
+	var dbDialector gorm.Dialector
+	dsn := getDsn(rw, conf)
+	dbDialector = mysql.Open(dsn)
+	return dbDialector, nil
+}
+
+func getDsn(rw string, conf Conf) string {
+	if rw == "r" {
+		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=false&loc=Local",
+			conf.Read.User,
+			conf.Read.Pass,
+			conf.Read.Host,
+			conf.Read.Port,
+			conf.Read.DataBase,
+			conf.Read.Charset)
+	} else {
+		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=false&loc=Local",
+			conf.Write.User,
+			conf.Write.Pass,
+			conf.Write.Host,
+			conf.Write.Port,
+			conf.Write.DataBase,
+			conf.Write.Charset)
+	}
 }
